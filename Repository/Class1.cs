63aea783d40e7eef9e3bbf52aaefdeaa36545c48
Hemant-finnid <hemant@finnid.in>
2024-04-23 12:14:38 +0530

    Add project files.

---
+++ Repository/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Repository
+{
+    public class Class1
+    {
+        public int Sum(int x,int y)
+        {
+            return x + y;
+        }
+        public int divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ TEST.API/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Repository;
+
+namespace TEST.API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 obj = new Class1();
+            int res = obj.Sum(2,3);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Class1 obj = new Class1();
+            int res = obj.divide(9, 0);
+        }
+    }
+}
